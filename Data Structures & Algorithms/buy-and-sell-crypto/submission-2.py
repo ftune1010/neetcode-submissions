bb61dc87d907cdfe404f825f17159b8efafe5683
Oyepitan Fortune <oyepitan10@gmail.com>
2026-06-10 04:44:02 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit=0
+        high=low=prices[0]
+        for i in range(1,len(prices)):
+            if prices[i]<low:
+                high=low=prices[i]
+            elif prices[i]>high:
+                high=prices[i]
+                profit=max(profit,high-low)
+        return profit
+        
